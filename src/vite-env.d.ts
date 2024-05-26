c5f32743e9221cc64673825454ba3b053e5039a2
Alejandro Coca <alxmcr@outlook.com>
2024-05-26 18:22:13 -0400

    wip

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,10 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_PORT: number;
   readonly VITE_API_VERSION: string;
   readonly VITE_API_BASE_URL: string;
-  // Mapbox
-  readonly VITE_APP_MAPBOX_MODE_DISPLAY: string;
-  readonly VITE_APP_MAPBOX_ACCESS_TOKEN: string;
-  readonly VITE_APP_MAPBOX_ACCESS_STYLE_MAP: string;
 }
 
 interface ImportMeta {
