cc309d5a6efa551dd63cc0c7ed3408845728acb7
Alejandro Coca <alxmcr@outlook.com>
2024-05-26 23:42:32 -0400

    wip

--- src/components/home-page/sections/HeroSection.tsx
+++ src/components/home-page/sections/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
   return (
     <BaseSection id="hero" isWidthFull>
       <div className="flex h-screen flex-col items-center">
-        <div className="flex h-screen flex-col items-start justify-center bg-right px-4 md:w-full md:px-10 lg:mx-auto lg:w-[1028px] lg:bg-[url('/assets/illustrations/tablet/building-002.svg')] lg:bg-no-repeat lg:px-0">
+        <div className="flex h-screen flex-col items-start justify-center bg-right px-4 md:w-full md:bg-[url('/assets/illustrations/tablet/building-002.svg')] md:bg-no-repeat md:px-10 lg:mx-auto lg:w-[1028px] lg:bg-none lg:bg-no-repeat lg:px-0">
           <CardHeroInfo />
         </div>
       </div>
