7f0f659bcb3275ebe96d5d4fbd2480d5189706ae
Alejandro Coca <alxmcr@outlook.com>
2024-05-27 18:23:08 -0400

    wip

--- src/pages/ApartmentsListingsPage.tsx
+++ src/pages/ApartmentsListingsPage.tsx
@@ -1,12 +1,15 @@
+import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
 import { useLocalApartments } from '../hooks/apartments/local/useLocalApartments';
 
 export default function ApartmentsListingsPage() {
   const { apartments, errorApartments, loadingApartments } = useLocalApartments();
+  const { apartment, errorApartment, loadingApartment } = useLocalApartment('1');
 
   return (
     <main>
       <h1>Listings</h1>
       <pre>{JSON.stringify({ apartments, errorApartments, loadingApartments }, null, 2)}</pre>
+      <pre>{JSON.stringify({ apartment, errorApartment, loadingApartment }, null, 2)}</pre>
     </main>
   );
 }
