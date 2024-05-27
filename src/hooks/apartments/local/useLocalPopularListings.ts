2f0a2e74f4495fb0a86dd31a1a9f61846c04a77c
Alejandro Coca <alxmcr@outlook.com>
2024-05-27 19:46:27 -0400

    wip

---
+++ src/hooks/apartments/local/useLocalPopularListings.ts
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { Apartment } from '../../../@types/service/apartmentTypes';
+import { LoadingStates } from '../../../@types/service/enumsService';
+import { LocalApartmentsServiceImpl } from '../../../services/apartments/local/LocalApartmentsServiceImpl';
+
+export const useLocalPopularListings = () => {
+  const [popularApartments, setPopularApartments] = React.useState<Apartment[]>([]);
+  const [errorPopularApartments, setErrorPopularApartments] = React.useState<Error | null>(null);
+  const [loadingPopularApartments, setLoadingPopularApartments] = React.useState(LoadingStates.IDLE);
+
+  useEffect(() => {
+    const fetchApartments = async () => {
+      try {
+        setLoadingPopularApartments(LoadingStates.PENDING);
+
+        const service = new LocalApartmentsServiceImpl();
+        const apiApartments = await service.findPopularListings();
+
+        setPopularApartments(apiApartments);
+        setLoadingPopularApartments(LoadingStates.SUCCESS);
+      } catch (error) {
+        setLoadingPopularApartments(LoadingStates.ERROR);
+        if (error instanceof Error && error.name === 'AbortError') {
+          setErrorPopularApartments(null);
+        } else if (error instanceof Error) {
+          setErrorPopularApartments(error);
+        }
+      }
+    };
+
+    // Fetch
+    fetchApartments();
+  }, []);
+
+  return { popularApartments, loadingPopularApartments, errorPopularApartments };
+};
