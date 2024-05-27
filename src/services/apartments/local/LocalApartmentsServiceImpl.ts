e9e2026cc45693c0c78b4e70cdda8f9442c9dfaa
Alejandro Coca <alxmcr@outlook.com>
2024-05-27 17:48:22 -0400

    wip

--- src/services/apartments/local/LocalApartmentsServiceImpl.ts
+++ src/services/apartments/local/LocalApartmentsServiceImpl.ts
@@ -1,7 +1,13 @@
-import { mapperListLocalApartmentsDataToListApt } from '../../../@mappers/mapperLocalApartments';
+import {
+  mapperListLocalApartmentsDataToListApt,
+  mapperLocalApartmentDataToApartment,
+} from '../../../@mappers/mapperLocalApartments';
 import { Apartment } from '../../../@types/service/apartmentTypes';
 import { LocalApartmentData } from '../../../@types/service/serviceTypes';
 import mockLocalApartments from '../../../mocks/data/local/mock-local-apartments.json';
+import mockLocalApartment001 from '../../../mocks/data/local/apartments/mock-local-apart-001.json';
+import mockLocalApartment002 from '../../../mocks/data/local/apartments/mock-local-apart-002.json';
+import mockLocalApartment003 from '../../../mocks/data/local/apartments/mock-local-apart-003.json';
 import { ILocalApartmentsService } from './ILocalApartmentsService';
 
 export class LocalApartmentsServiceImpl implements ILocalApartmentsService {
@@ -22,7 +28,17 @@ export class LocalApartmentsServiceImpl implements ILocalApartmentsService {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         try {
-          resolve(null);
+          switch (id) {
+            case 1:
+              resolve(mapperLocalApartmentDataToApartment(mockLocalApartment001 as LocalApartmentData));
+              break;
+            case 2:
+              resolve(mapperLocalApartmentDataToApartment(mockLocalApartment002 as LocalApartmentData));
+              break;
+            case 3:
+              resolve(mapperLocalApartmentDataToApartment(mockLocalApartment003 as LocalApartmentData));
+              break;
+          }
         } catch (error) {
           reject(error);
         }
