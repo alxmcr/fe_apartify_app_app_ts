4e2ea7a5f8397f1d514c698a53a7209da7ef7774
Alejandro Coca <alxmcr@outlook.com>
2024-05-28 15:04:43 -0400

    wip

--- src/@types/service/serviceTypes.ts
+++ src/@types/service/serviceTypes.ts
@@ -8,6 +8,7 @@ export type LocalAttributeData = {
   fe_feature: number;
   fe_type: string;
   fe_name: string;
+  fe_icon_code: string;
   fe_icon_url: string;
   fe_icon_color: string;
   fe_is_in_card: boolean;
