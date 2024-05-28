c9b5c27efdc8af921ca84bf51383481ea1f2574f
Alejandro Coca <alxmcr@outlook.com>
2024-05-28 16:23:59 -0400

    wip

---
+++ src/components/@icons/24x24/Icon24x24FloorPlan.tsx
@@ -0,0 +1,21 @@
+export default function Icon24x24FloorPlan() {
+  return (
+    <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_198_682)">
+        <path
+          d="M0 0.0265V24H16.925V17.8915H24V0L0 0.0265ZM22.925 10.708H21.19V11.783H22.925V16.816H16.925V15.627H15.85V22.927H5.672V21.7385H4.6V22.925H1.0755V17.8915H4.6V19.06H5.6745V15.6195H4.6V16.8165H1.0755V11.7835H4.6V12.986H5.6745V11.7835H7.841V10.7085H1.075V1.1H12.5965V10.708H10.45V11.783H15.85V12.978H16.925V11.783H18.625V10.708H13.671V1.0755H22.9245L22.925 10.708Z"
+          fill="#6A06FA"
+        />
+        <path
+          d="M16.925 24H0V0.0265L24 0V17.8915H16.925V24ZM1.075 22.9255H15.85V16.8165H22.925V1.076L1.075 1.1V22.9255Z"
+          fill="#6A06FA"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_198_682">
+          <rect width={24} height={24} fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
