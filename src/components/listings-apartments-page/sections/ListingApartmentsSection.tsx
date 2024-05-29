d30fa6b857b51e49fe53d515d771ae7026c6500f
Alejandro Coca <alxmcr@outlook.com>
2024-05-29 02:16:17 -0400

    wip

--- src/components/listings-apartments-page/sections/ListingApartmentsSection.tsx
+++ src/components/listings-apartments-page/sections/ListingApartmentsSection.tsx
@@ -1,10 +1,10 @@
-import BoxListingApartments from "../../@apartments/BoxListingApartments";
-import BaseSection from "../../BaseSection";
+import BoxListingApartments from '../../@apartments/BoxListingApartments';
+import BaseSection from '../../BaseSection';
 
 export default function ListingApartmentsSection() {
   return (
     <BaseSection>
-      <div className="flex flex-col gap-[26px] py-5 md:gap-[46px]">
+      <div className="flex h-screen flex-col gap-[26px] py-5 md:gap-[46px]">
         <h2 className="text-[26px] font-bold md:text-[28px] lg:text-[36px]">
           {'Latest '}
           <span className="border border-x-0 border-t-0 border-electric-violet-600 font-bold  text-electric-violet-600">
