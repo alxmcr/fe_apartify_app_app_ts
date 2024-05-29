26a35acd65a5c11e6f92cb81ee3664bc2fc8961d
Alejandro Coca <alxmcr@outlook.com>
2024-05-29 00:09:49 -0400

    wip

--- src/components/@apartments/CardApartment.tsx
+++ src/components/@apartments/CardApartment.tsx
@@ -6,13 +6,23 @@ type Props = {
 
 export default function CardApartment({ apartment }: Props) {
   return (
-    <article className="h-[402px] w-[288px] md:h-[428px] md:w-[310px] lg:h-[444px] lg:w-[348px]">
-      <img src={apartment?.ap_url} alt={`Photo by ${apartment.ap_description}`} />
-      <div>
-        <h3>{apartment?.ap_cost_offer}</h3>
-        <span>{apartment?.ap_cost_list}</span>
+    <article className="h-[402px] w-[288px] border md:h-[428px] md:w-[310px] lg:h-[444px] lg:w-[348px]">
+      <img
+        src={apartment?.ap_url}
+        alt={`Photo by ${apartment.ap_description}`}
+        className="h-[182px] w-full rounded-2xl bg-cover md:h-[204px] lg:h-[222px]"
+      />
+      <div className="flex items-center justify-between">
+        <h3 className="font-groteskspace text-[18px] font-bold text-electric-violet-600">
+          {apartment?.ap_cost_offer}
+        </h3>
+        <span className="font-groteskspace text-[18px] font-bold text-electric-violet-200 line-through">
+          {apartment?.ap_cost_list}
+        </span>
       </div>
-      <p>{apartment?.ap_street_name} {apartment?.ap_ext_number}</p>
+      <p>
+        {apartment?.ap_street_name} {apartment?.ap_ext_number}
+      </p>
       <div></div>
     </article>
   );
