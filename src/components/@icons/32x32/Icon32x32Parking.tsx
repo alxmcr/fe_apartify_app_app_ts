5293e4f175141bb0754df953b023c66f6ebcb260
Alejandro Coca <alxmcr@outlook.com>
2024-05-28 23:12:59 -0400

    wip

---
+++ src/components/@icons/32x32/Icon32x32Parking.tsx
@@ -0,0 +1,68 @@
+export default function Icon32x32Parking() {
+  return (
+    <svg width={32} height={32} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M22.0375 8.244C21.917 7.8045 21.5175 7.5 21.062 7.5C18.944 7.5 13.056 7.5 10.938 7.5C10.4825 7.5 10.083 7.8045 9.9625 8.244C9.3935 10.3155 7.8335 16 7.8335 16H24.1665C24.1665 16 22.6065 10.3155 22.0375 8.244Z"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M26.4535 19C26.4145 18.77 26.351 18.5445 26.264 18.327C26.068 17.8365 25.789 17.139 25.5875 16.636C25.434 16.252 25.062 16 24.6485 16C21.721 16 10.279 16 7.3515 16C6.938 16 6.566 16.252 6.4125 16.636C6.211 17.139 5.932 17.8365 5.736 18.327C5.58 18.7165 5.5 19.132 5.5 19.5515V21.988C5.5 22.547 5.953 23 6.512 23C8.596 23 13.547 23 18 23"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.7915 18.7915H7.953C8.416 18.7915 8.839 19.053 9.046 19.467C9.2925 19.96 9.5835 20.5415 9.5835 20.5415"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 15.2915C8 15.2915 7.1545 13.5415 6.0835 13.5415"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24.1665 15.2915C24.1665 15.2915 25.012 13.5415 26.0835 13.5415"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10.75 23.25H7.25V25.09C7.25 25.331 7.346 25.5625 7.5165 25.7335C7.6875 25.904 7.919 26 8.16 26C8.6775 26 9.3225 26 9.84 26C10.081 26 10.3125 25.904 10.4835 25.7335C10.654 25.5625 10.75 25.331 10.75 25.09C10.75 24.289 10.75 23.25 10.75 23.25Z"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M28 20.5C28 19.6715 27.3285 19 26.5 19C25.0705 19 22.9295 19 21.5 19C20.6715 19 20 19.6715 20 20.5C20 21.51 20 22.864 20 24.0255C20 25.502 20.866 26.841 22.2125 27.446C23.137 27.862 24 28.25 24 28.25C24 28.25 24.863 27.862 25.7875 27.446C27.134 26.841 28 25.502 28 24.0255V20.5Z"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22.375 23.374L23.625 24.374L25.625 22.624"
+        stroke="currentColor"
+        strokeMiterlimit={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
--- src/pages/ApartmentsListingsPage.tsx
+++ src/pages/ApartmentsListingsPage.tsx
@@ -1,4 +1,6 @@
 import App24x24Icon from '../components/@icons/24x24/App24x24Icon';
+import App28x28Icon from '../components/@icons/28x28/App28x28Icon';
+import App32x32Icon from '../components/@icons/32x32/App32x32Icon';
 import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
 import { useLocalApartments } from '../hooks/apartments/local/useLocalApartments';
 import { useLocalLatestListings } from '../hooks/apartments/local/useLocalLatestListing';
@@ -20,9 +22,27 @@ export default function ApartmentsListingsPage() {
         <App24x24Icon iconCode="icon-bathroom" />
         <App24x24Icon iconCode="icon-bedroom" />
         <App24x24Icon iconCode="icon-parking" />
-      </div>
-      <div className="text-purple-500">
         <App24x24Icon iconCode="icon-key-vertical-rounded" />
+      </div>
+      <br />
+
+      <hr />
+      <div className="text-cerise-400">
+        <App28x28Icon iconCode="icon-floor-plan" />
+        <App28x28Icon iconCode="icon-bathroom" />
+        <App28x28Icon iconCode="icon-bedroom" />
+        <App28x28Icon iconCode="icon-parking" />
+        <App28x28Icon iconCode="icon-key-vertical-rounded" />
+      </div>
+      <br />
+
+      <hr />
+      <div className="text-cerise-400">
+        <App32x32Icon iconCode="icon-floor-plan" />
+        <App32x32Icon iconCode="icon-bathroom" />
+        <App32x32Icon iconCode="icon-bedroom" />
+        <App32x32Icon iconCode="icon-parking" />
+        <App32x32Icon iconCode="icon-key-vertical-rounded" />
       </div>
       <br />
 
