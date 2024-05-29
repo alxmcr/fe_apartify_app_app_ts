ddd2a3f8a46246025a0c4df2e4ececf8f23fe7c2
Alejandro Coca <alxmcr@outlook.com>
2024-05-29 00:46:36 -0400

    wip

--- src/components/home-page/sections/PopularApartmentsSection.tsx
+++ src/components/home-page/sections/PopularApartmentsSection.tsx
@@ -1,3 +1,12 @@
+import { useLocalPopularListings } from '../../../hooks/apartments/local/useLocalPopularListings';
+import BoxGroupApartments from '../../@apartments/BoxGroupApartments';
+
 export default function PopularApartmentsSection() {
-  return <section></section>;
+  const { popularApartments, loadingPopularApartments, errorPopularApartments } = useLocalPopularListings();
+
+  return (
+    <section>
+      <BoxGroupApartments apartments={popularApartments} />
+    </section>
+  );
 }
