3b06711c94bc81f363e97a2613ee3dd37821216a
Alejandro Coca <alxmcr@outlook.com>
2024-05-30 00:38:56 -0400

    wip

--- src/pages/ApartmentPage.tsx
+++ src/pages/ApartmentPage.tsx
@@ -1,8 +1,9 @@
 import { useParams } from 'react-router-dom';
-import MainContentApartment from '../components/apartment-page/MainContentApartment';
-import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
 import { LoadingStates } from '../@types/service/enumsService';
+import MainContentApartment from '../components/apartment-page/MainContentApartment';
 import ApartmentDescriptionSection from '../components/apartment-page/sections/ApartmentDescriptionSection';
+import ApartmentFeaturesDetailsSection from '../components/apartment-page/sections/ApartmentFeaturesDetailsSection';
+import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
 
 export default function ApartmentPage() {
   const { id } = useParams();
@@ -21,6 +22,7 @@ export default function ApartmentPage() {
       <main className="flex flex-col gap-8">
         <MainContentApartment apartment={apartment} />
         <ApartmentDescriptionSection apartment={apartment} />
+        <ApartmentFeaturesDetailsSection apartment={apartment} />
       </main>
     );
   }
