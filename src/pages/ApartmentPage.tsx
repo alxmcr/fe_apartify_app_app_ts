ecd2d746704bc62b46e5d76d684244434626bf70
Alejandro Coca <alxmcr@outlook.com>
2024-05-29 23:23:32 -0400

    wip

--- src/pages/ApartmentPage.tsx
+++ src/pages/ApartmentPage.tsx
@@ -1,14 +1,27 @@
 import { useParams } from 'react-router-dom';
+import MainContentApartment from '../components/apartment-page/MainContentApartment';
 import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
+import { LoadingStates } from '../@types/service/enumsService';
 
 export default function ApartmentPage() {
   const { id } = useParams();
   const { apartment, errorApartment, statusApartment } = useLocalApartment(id);
 
-  return (
-    <div>
-      <h1>Apartment...</h1>
-      <pre>{JSON.stringify({ apartment, errorApartment, statusApartment }, null, 2)}</pre>
-    </div>
-  );
+  if (LoadingStates.PENDING === statusApartment) {
+    return <h2>Loading apartment data...</h2>;
+  }
+
+  if (LoadingStates.ERROR === statusApartment && errorApartment) {
+    return <h2>{errorApartment.message}</h2>;
+  }
+
+  if (LoadingStates.SUCCESS === statusApartment && apartment !== null) {
+    return (
+      <main>
+        <MainContentApartment apartment={apartment} />
+      </main>
+    );
+  }
+
+  return null;
 }
