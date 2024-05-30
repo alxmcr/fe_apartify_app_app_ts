862a348522b3fc94877b1f776149f284d9f58470
Alejandro Coca <alxmcr@outlook.com>
2024-05-29 22:19:05 -0400

    wip

--- src/pages/ApartmentPage.tsx
+++ src/pages/ApartmentPage.tsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom';
+import { useLocalApartment } from '../hooks/apartments/local/useLocalApartment';
+
 export default function ApartmentPage() {
+  const { id } = useParams();
+  const { apartment, errorApartment, loadingApartment } = useLocalApartment(id);
+
   return (
     <main>
       <h1>Apartment...</h1>
+      <pre>{JSON.stringify({ apartment, errorApartment, loadingApartment }, null, 2)}</pre>
     </main>
   );
 }
